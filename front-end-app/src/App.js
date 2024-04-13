7a738e3eaa5df488ce1f25588b9dcda204a4d06a
mittallakshayy <becausewecode@gmail.com>
2024-04-13 16:33:13 -0700

    Adding export to csv functionality

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import DisplayTable from "./components/DisplayTable";
 import HomeIcon from "@mui/icons-material/Home";
 import { Menu, MenuItem } from "@mui/material";
+import DownloadIcon from '@mui/icons-material/Download';
+import {utils,writeFile} from "xlsx";
 
 function App() {
   const [data, setData] = useState([]);
@@ -22,7 +24,7 @@ function App() {
 
   const fetchData = useCallback(async () => {
     try {
-      const response = await fetch("http://localhost:3003/healthshare/alldata", {
+      const response = await fetch("http://localhost:3001/healthshare/alldata", {
         mode: "cors",
       });
       const result = await response.json();
@@ -39,7 +41,7 @@ function App() {
 
   const handleQuery = async (query) => {
     try {
-      const url = new URL("http://localhost:3003/healthshare/querydata");
+      const url = new URL("http://localhost:3001/healthshare/querydata");
       url.searchParams.append("query", query);
       const response = await fetch(url, {
         mode: "cors",
@@ -67,6 +69,17 @@ function App() {
       console.error("There was a problem with the fetch operation:", error);
     }
   };
+  const handleExport = async ()=> {
+    const headings = [['Id', 'Text','Created at', 'Source' ]];
+    const wb = utils.book_new();
+    const ws = utils.json_to_sheet([]);
+    utils.sheet_add_aoa(ws, headings);
+    utils.sheet_add_json(ws,data,{origin: "A2",skipHeader:true});
+    utils.book_append_sheet(wb, ws , "Data");
+    writeFile(wb, "Data.xlsx");
+
+  };
+  
   return (
     <div className="m-5 mb-5">
       <div className="input-group p-2 mb-5">
@@ -110,6 +123,7 @@ function App() {
           >
             Sort By
           </button>
+          <DownloadIcon className="mx-3" onClick ={handleExport}></DownloadIcon>
           <Menu
             anchorEl={anchorEl}
             open={Boolean(anchorEl)}
