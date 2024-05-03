5d1ff44c9d25f757a51d795a3dfd4b257ac438da
Parth Panchal <parth6606@gmail.com>
2024-05-02 22:22:57 -0700

    integrated cockroach db - require env file

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const client = new Client({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: 5432,
+  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false,
   }, // default Postgres port
